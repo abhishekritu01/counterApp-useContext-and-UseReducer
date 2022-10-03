a90ff0907170932ec4fcb8dce9cfeed11a800f4b
abhishek <abhishekritu810219@gmail.com>
2022-10-03 13:00:18 +0530

    useReducer hook

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import './App.css'
+import React, { useContext } from "react";
+import { CounterProvider, CounterContext } from "./Context";
+
+
+
+function Counter() {
+  const { state, dispatch } = useContext(CounterContext);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h5>Count: {state.count}</h5>
+      <button onClick={() => dispatch({ type: "increment" })}>+</button>
+      <button onClick={() => dispatch({ type: "decrement" })}>-</button>
     </div>
   );
 }
 
-export default App;
+const App = () => {
+  return (
+    <div className="App">
+      <CounterProvider>
+        <Counter />
+      </CounterProvider>
+    </div>
+  )
+}
+
+export default App
